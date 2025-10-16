6229b82b2e8884bd1f99697df0f1a46cc9e543b4
Adam Yogisyah Putra <adamyogisp@gmail.com>
2025-10-17 01:46:10 +0700

    cors update

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,31 @@ import (
 	"gin-api/package/routes"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.HouseListing(r)
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	log.Printf("Starting server on port %s\n", port)
+
+	corsHandler := handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)
+
+	err := http.ListenAndServe(":"+port, corsHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
